b9d8b9f73d8cf3f6e3b9101c56889db557cab630
euan.gwd <euan@amwic.co.za>
2018-04-28 16:55:36 +0200

    Setup Map Centering

--- src/Components/Content/MapContainer.js
+++ src/Components/Content/MapContainer.js
@@ -3,20 +3,28 @@ import { ClientContext } from "../App";
 import { Map, Marker, InfoWindow, GoogleApiWrapper } from "google-maps-react";
 
 export class MapContainer extends Component {
-  state = { showingInfoWindow: false, activeMarker: {}, selectedPlace: {} };
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {},
+    centerMap: { lat: -33.921829646, lng: 18.420998316 }
+  };
 
-  onMarkerClick = (props, marker, e) =>
+  onMarkerClick = (props, marker, e) => {
     this.setState({
       selectedPlace: props,
       activeMarker: marker,
-      showingInfoWindow: true
+      showingInfoWindow: true,
+      centerMap: props.mapCenter
     });
+  };
 
   onMapClicked = props => {
     if (this.state.showingInfoWindow) {
       this.setState({
         showingInfoWindow: false,
-        activeMarker: null
+        activeMarker: null,
+        centerMap: { lat: -33.921829646, lng: 18.420998316 }
       });
     }
   };
@@ -31,13 +39,16 @@ export class MapContainer extends Component {
             zoom={10}
             style={{ height: "100%", position: "relative", width: "100%" }}
             initialCenter={{ lat: -33.921829646, lng: 18.420998316 }}
-            center={{ lat: -33.921829646, lng: 18.420998316 }}
+            center={this.state.centerMap}
             onClick={this.onMapClicked}
           >
             {context.customers.map(customer => (
               <Marker
                 name={`${customer.Firstname} ${customer.Surname}`}
-                position={{ lat: customer.Lat, lng: customer.Long }}
+                position={{
+                  lat: customer.Lat,
+                  lng: customer.Long
+                }}
                 key={customer.Id}
                 onClick={this.onMarkerClick}
               />
